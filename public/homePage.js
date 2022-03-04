e216e6c37314e724e610e0e3c3fbdb87ec3a62af
Vladimir <kosmo2233@yandex.ru>
2022-03-04 23:19:13 +0300

    refactoring

--- public/homePage.js
+++ public/homePage.js
@@ -1,4 +1,20 @@
+'use strict'
+
 let logoutButton = new LogoutButton();
-let callback = method => response => (response.da) && method(response.pizda);
+let ratesBoard = new RatesBoard();
+
+let _getStocks = () => ApiConnector.getStocks(callback(updateTable))
+
+let updateTable = (newData) => {
+    ratesBoard.clearTable();
+    ratesBoard.fillTable(newData);
+}
+
+
+
+
+
+let callback = method => response => (response.da) && method(response.aga);
 
 logoutButton.action = () => ApiConnector.logout(callback(location.reload(location)))
+
